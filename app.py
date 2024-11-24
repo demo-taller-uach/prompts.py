7889fae7fe5e1595f990f05190f1b528ae950cf9
demo-taller-uach <demo.taller.uach@gmail.com>
2024-11-24 17:52:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from openai import OpenAI
 openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
-
-stream = client.chat.completions.create(
-        model="gpt-4o-mini",  
-        messages=[
-            {"role": "system", "content": "You are an assistant."},
-            {"role": "user", "content": prompt}
-        ],
-        max_tokens=800,
-        temperature=0,
-    )
-respuesta = stream.choices[0].message.content
+def asistente(prompt):
+        stream = client.chat.completions.create(
+                model="gpt-4o-mini",  
+                messages=[
+                    {"role": "system", "content": "You are an assistant."},
+                    {"role": "user", "content": prompt}
+                ],
+                max_tokens=800,
+                temperature=0,
+            )
+        respuesta = stream.choices[0].message.content
+        return respuesta
 def page_2():
     st.title("Page 2")
     prompt = st.chat_input("What is up?")
