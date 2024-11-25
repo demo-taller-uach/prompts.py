8cfb69f29bb442a0e5f3199e4e0763f57fe2b615
demo-taller-uach <demo.taller.uach@gmail.com>
2024-11-24 18:31:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ openai_api_key = st.secrets["api_key"]
 client = OpenAI(api_key=openai_api_key)
 
 
-color = st.select_slider(
+color = st.sidebar.select_slider(
     "Temperatura",
     options=[
     0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0
