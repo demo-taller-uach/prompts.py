247e5eb629049ebf1b87672cb4657e6498d49470
demo-taller-uach <demo.taller.uach@gmail.com>
2024-11-24 18:04:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,25 @@ from openai import OpenAI
 openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
 client = OpenAI(api_key=openai_api_key)
-def asistente(prompt):
+def asistente1(prompt):
         stream = client.chat.completions.create(
                 model="gpt-4o-mini",  
                 messages=[
                     {"role": "system", "content": "You are an assistant."},
                     {"role": "user", "content": prompt}
+                ],
+                max_tokens=800,
+                temperature=0,
+            )
+        respuesta = stream.choices[0].message.content
+        return respuesta
+
+def asistente2(instruc,prompt):
+        stream = client.chat.completions.create(
+                model="gpt-4o-mini",  
+                messages=[
+                    {"role": "system", "content": "You are an assistant."},
+                    {"role": "user", "content": instruc + ": " + prompt}
                 ],
                 max_tokens=800,
                 temperature=0,
@@ -23,9 +36,20 @@ def page_2():
         st.stop()
     with st.chat_message("user"):
         st.markdown(prompt)
-    respuesta = asistente(prompt)
+    respuesta = asistente1(prompt)
     with st.chat_message("assistant"):
         st.write(respuesta)
 
-pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
+def page_3():
+    st.title("Page 2")
+    prompt = st.chat_input("Escribe tu pregunta")
+    if prompt==None:
+        st.stop()
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.text_input= "Use emojis to improve your answers"
+    respuesta = asistente1(prompt)
+    with st.chat_message("assistant"):
+        st.write(respuesta)
+pg = st.navigation([st.Page("page_1.py"), st.Page(page_2), st.Page(page_3)])
 pg.run()
